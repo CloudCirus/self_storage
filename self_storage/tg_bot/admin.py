fe5d50b4430344e1d7b0d374775694bddce607fe
Cloud <smirnovceo@yandex.ru>
2021-11-25 22:58:16 +0500

    Register models in admin

--- self_storage/tg_bot/admin.py
+++ self_storage/tg_bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(Storage)
+admin.site.register(Storage_item)
+admin.site.register(Order)
