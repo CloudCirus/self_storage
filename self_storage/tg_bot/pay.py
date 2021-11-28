4b3f56581f7bfc85a054426adc622e165ffae655
Oleg Rudakov <nefoser@gmail.com>
2021-11-28 20:46:11 +0300

    Update pay.py

--- self_storage/tg_bot/pay.py
+++ self_storage/tg_bot/pay.py
@@ -1,34 +1,61 @@
-import requests
-import json
+import telegram
 from environs import Env
+from telegram import Update, LabeledPrice
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    Filters,
+    PreCheckoutQueryHandler,
+    CallbackContext,
+    CallbackQueryHandler,
+)
+
+env = Env()
+env.read_env()
+provider_token = env.str('PROVIDER_TOKEN')
+token = env.str('TG_TOKEN')
+chat_id = env.str('BOT_ID')
+
+bot = telegram.Bot(token=token)
 
 
-def send_invoice(chat_id, token, provider_token):
-    method="sendInvoice"
-    url = f"https://api.telegram.org/bot{token}/{method}"
-    prices = json.dumps([{
-        "label": "руб",
-        "amount": 10000
-    }])
-    params = {
-        "chat_id": chat_id,
-        "title": "SELF STORAGE",
-        "description": "Услуги аренды",
-        "payload": "YourPayload",
-        "provider_token": provider_token,
-        "currency": "RUB",
-        "start parameter": "test",
-        "prices": prices
-    }
-
-    response = requests.post(url, params=params)
-    response.raise_for_status()
+def send_invoice(update: Update, context: CallbackContext):
+    prices = [LabeledPrice(label="руб", amount=10000)]
+    title = "SELF STORAGE"
+    description = "Услуги аренды"
+    payload = "YourPayload"
+    currency = "RUB"
+    start_parameter = "test"
+    context.bot.sendInvoice(
+        chat_id,
+        title,
+        description,
+        payload,
+        provider_token,
+        start_parameter,
+        currency,
+        prices
+    )
 
 
-if __name__ == "__main__":
-    env = Env()
-    env.read_env()
-    provider_token = env.str('PROVIDER_TOKEN')
-    token = env.str('TG_TOKEN')
-    chat_id = env.str('BOT_ID')
-    send_invoice(chat_id, token, provider_token)
+def precheckout_callback(update: Update, context: CallbackContext):
+    query = update.pre_checkout_query
+
+    if query.invoice_payload != "YourPayload":
+        bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=False,
+                                      error_message="Что то пошло не так")
+    else:
+        bot.answer_pre_checkout_query(pre_checkout_query_id=query.id, ok=True)
+
+
+def successful_payment_callback(update: Update, context: CallbackContext):
+    update.message.reply_text("Все готово.")
+
+
+updater = Updater(token)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CallbackQueryHandler(send_invoice))
+dispatcher.add_handler(PreCheckoutQueryHandler(precheckout_callback))
+dispatcher.add_handler(MessageHandler(Filters.successful_payment, successful_payment_callback))
+updater.start_polling()
